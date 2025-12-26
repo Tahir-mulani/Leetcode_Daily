01636486db7409c2b7c3310ec26e5a8a7d9262ed
Tahir-07 <tahirmulani7447@gmail.com>
2025-12-26 21:19:57 +0530

    sum of unique element leetcode 1748 question solved

---
+++ SumOfUniqueElementleetcode1748APP.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class SumOfUniqueElementleetcode1748APP
+{
+	public static void main(String x[])
+	{
+		int nums[]={1,2,3,2};
+		int result = sumOfUnique(nums);
+		System.out.println(result);
+		
+	}
+	public static int sumOfUnique(int[] nums)
+	{
+		Map<Integer,Integer> map = new HashMap<>();
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
+		}
+		
+		int sum = 0;
+		
+		for(int key : map.keySet())
+		{
+			if(map.get(key)== 1)
+			{
+				sum += key;
+			}
+		}
+		
+		return sum;
+	}
+}
+		
+			
