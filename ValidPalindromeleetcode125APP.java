05c4d677615541a88e34704b34260f83091809fc
Tahir-07 <tahirmulani7447@gmail.com>
2026-02-18 11:28:07 +0530

    Add solution for LeetCode 125 - Valid Palindrome

---
+++ ValidPalindromeleetcode125APP.java
@@ -0,0 +1,41 @@
+public class ValidPalindromeleetcode125APP {
+
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+
+        ValidPalindromeleetcode125APP obj = new ValidPalindromeleetcode125APP();
+
+        String test1 = "A man, a plan, a canal: Panama";
+        String test2 = "race a car";
+        String test3 = "0P";
+
+        System.out.println(test1 + " -> " + obj.isPalindrome(test1));
+        System.out.println(test2 + " -> " + obj.isPalindrome(test2));
+        System.out.println(test3 + " -> " + obj.isPalindrome(test3));
+    }
+}
