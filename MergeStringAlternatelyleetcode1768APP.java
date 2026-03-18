364408c2d89fe8aa19e476a40376c1cfd92310bb
Tahir-07 <tahirmulani7447@gmail.com>
2026-03-18 17:15:16 +0530

    merged string alternate leetcode 1768

---
+++ MergeStringAlternatelyleetcode1768APP.java
@@ -0,0 +1,31 @@
+public class MergeStringAlternatelyleetcode1768APP
+{
+	public static void main(String x[])
+	{
+		String word1 = 	"abc",word2 = "pqr";
+		System.out.println( "before merged String -> "+word1+","+word2);
+		String result = mergeAlternately(word1,word2);
+		
+		System.out.println("alternate merged String -> " +result);
+	}
+    public static String mergeAlternately(String word1, String word2)
+	{
+         StringBuilder result = new StringBuilder();
+         int i=0,j=0;
+
+         while(i<word1.length() || j<word2.length()){
+
+            if(i<word1.length()){
+                result.append(word1.charAt(i));
+                i++;
+            }
+
+            if(j<word2.length()){
+                result.append(word2.charAt(j));
+                j++;
+            }
+         }
+
+         return result.toString();
+    }
+}
